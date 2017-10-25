64ccf654ef25d81f9fc467e8ce12e23913512bac
Steve Jorgensen <stevej@stevej.name>
2017-10-25 00:54:09 -0700

    Add inline comments about escaping/unescaping

--- lib/carton_db/escaping.rb
+++ lib/carton_db/escaping.rb
@@ -5,46 +5,58 @@ module CartonDb
 
   module Escaping
 
+    # Most importantly, escape values that would interfere with
+    # the use of newline and tab delimiters. Also escape the
+    # characters most likely to cause problems when dumping
+    # contents to a terminal console or viewing/editing content
+    # in a text editor.
+    # Use fairly short, commonly understood, ANSI-C backslash
+    # quoting sequences. Not using the slightly shorter
+    # "\c<x>" control character sequences since those are much
+    # less wll known.
+
     ESCAPING_MAP = {
-      "\u0000" => '\x00',
-      "\u0001" => '\x01',
-      "\u0002" => '\x02',
-      "\u0003" => '\x03',
-      "\u0004" => '\x04',
-      "\u0005" => '\x05',
-      "\u0006" => '\x06',
-      "\u0007" => '\a',
-      "\u0008" => '\b',
-      "\u0009" => '\t',
-      "\u000A" => '\n',
-      "\u000B" => '\v',
-      "\u000C" => '\f',
-      "\u000D" => '\r',
-      "\u000E" => '\x0E',
-      "\u000F" => '\x0F',
-      "\u0010" => '\x10',
-      "\u0011" => '\x11',
-      "\u0012" => '\x12',
-      "\u0013" => '\x13',
-      "\u0014" => '\x14',
-      "\u0015" => '\x15',
-      "\u0016" => '\x16',
-      "\u0017" => '\x17',
-      "\u0018" => '\x18',
-      "\u0019" => '\x19',
-      "\u001A" => '\x1A',
-      "\u001B" => '\x1B',
-      "\u001C" => '\x1C',
-      "\u001D" => '\x1D',
-      "\u001E" => '\x1E',
-      "\u001F" => '\x1F',
-      "\u007F" => '\x7F',
-      "\\"     => "\\\\",
+      "\u0000" => '\x00',  # null
+      "\u0001" => '\x01',  # Ctrl+A
+      "\u0002" => '\x02',  # Ctrl+B
+      "\u0003" => '\x03',  # Ctrl+C
+      "\u0004" => '\x04',  # Ctrl+D
+      "\u0005" => '\x05',  # Ctrl+E
+      "\u0006" => '\x06',  # Ctrl+F
+      "\u0007" => '\a',    # alert/bell
+      "\u0008" => '\b',    # backspace
+      "\u0009" => '\t',    # tab
+      "\u000A" => '\n',    # newline
+      "\u000B" => '\v',    # vertical tab
+      "\u000C" => '\f',    # form feed
+      "\u000D" => '\r',    # carriage return
+      "\u000E" => '\x0E',  # Ctrl+N
+      "\u000F" => '\x0F',  # Ctrl+O
+      "\u0010" => '\x10',  # Ctrl+P
+      "\u0011" => '\x11',  # Ctrl+Q
+      "\u0012" => '\x12',  # Ctrl+R
+      "\u0013" => '\x13',  # Ctrl+S
+      "\u0014" => '\x14',  # Ctrl+T
+      "\u0015" => '\x15',  # Ctrl+U
+      "\u0016" => '\x16',  # Ctrl+V
+      "\u0017" => '\x17',  # Ctrl+W
+      "\u0018" => '\x18',  # Ctrl+X
+      "\u0019" => '\x19',  # Ctrl+Y
+      "\u001A" => '\x1A',  # Ctrl+Z
+      "\u001B" => '\x1B',  # escape
+      "\u001C" => '\x1C',  # file separator
+      "\u001D" => '\x1D',  # group separator
+      "\u001E" => '\x1E',  # record separator
+      "\u001F" => '\x1F',  # unit separator
+      "\u007F" => '\x7F',  # delete
+      "\\"     => "\\\\",  # backslash
     }.freeze
 
     UNESCAPING_MAP = ESCAPING_MAP.invert.freeze
 
     class << self
+      # Replace special characters with backslashed escape
+      # sequences.
       def escape(value)
         value.gsub(
           /[\x00-\x1F\x7F\\]/,
@@ -52,6 +64,8 @@ module CartonDb
         )
       end
 
+      # Replace backslashed escape sequences with special
+      # characters.
       def unescape(esc)
         esc.gsub( /\\(?:\\|x[01][0-9A-F]|x7F|[^x\\]|$)/ ) { |match|
           UNESCAPING_MAP.fetch match do
